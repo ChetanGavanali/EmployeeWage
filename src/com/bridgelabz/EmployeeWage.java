9541437cf10fc54079ceb488ea9319fccc71610c
ChetanGavanali <chetangavanali@gmail.com>
2022-06-20 22:56:43 +0530

    Added Total Working Hours

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,13 +9,17 @@ public class EmployeeWage {
 
         public static final int NUM_OF_WORKING_DAYS = 2;
 
+        public static final int MAX_HRS_IN_MONTH = 10;
+
     public static void main(String[] args) {
 
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++){
+        while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+
+        totalWorkingDays++;
 
         int empcheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empcheck) {
@@ -30,13 +34,11 @@ public class EmployeeWage {
 
         }
 
-
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        totalEmpWage += empWage;
-        System.out.println("Emp Wage: " + empWage);
-
+        totalEmpHrs += empHrs;
+            System.out.println("Days:" + totalWorkingDays + "EmpHr:" + empHrs);
 
     }
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
     System.out.println(" Total Emp Wage: " + totalEmpWage);
 }
 }
