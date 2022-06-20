096c483ca73497105bc2d07b69f949a3a844aecd
ChetanGavanali <chetangavanali@gmail.com>
2022-06-20 22:45:49 +0530

    Added Employee Wage Per Month

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -7,10 +7,15 @@ public class EmployeeWage {
         public static final int IS_FULL_TIME = 2;
         public static final int EMP_RATE_PER_HOUR = 20;
 
+        public static final int NUM_OF_WORKING_DAYS = 2;
+
     public static void main(String[] args) {
 
         int empHrs = 0;
         int empWage = 0;
+        int totalEmpWage = 0;
+
+        for (int day = 0; day < NUM_OF_WORKING_DAYS; day++){
 
         int empcheck = (int) Math.floor(Math.random() * 10) % 3;
         switch (empcheck) {
@@ -27,9 +32,11 @@ public class EmployeeWage {
 
 
         empWage = empHrs * EMP_RATE_PER_HOUR;
+        totalEmpWage += empWage;
         System.out.println("Emp Wage: " + empWage);
 
 
     }
+    System.out.println(" Total Emp Wage: " + totalEmpWage);
 }
-
+}
